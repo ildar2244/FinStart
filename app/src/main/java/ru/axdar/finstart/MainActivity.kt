1c4969f7615d566a28dbc70689cdf0840d317912
ildar2244 <ildar2244@gmail.com>
2020-10-08 16:27:35 +0300

    UX: перенес DrawerLayout and Navigation Graph в последний фрагмент BottomMenu.

--- app/src/main/java/ru/axdar/finstart/MainActivity.kt
+++ app/src/main/java/ru/axdar/finstart/MainActivity.kt
@@ -15,6 +15,7 @@ import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.android.material.navigation.NavigationView
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.inc_content_main.*
+import ru.axdar.finstart.screens.drawer_menu.NavigationDrawerFragment
 import ru.axdar.finstart.screens.portfolio.PortfolioFragment
 import ru.axdar.finstart.screens.trade_quotes.TradeQuotesFragment
 import ru.axdar.finstart.screens.transactions.TransactionsFragment
@@ -22,7 +23,7 @@ import ru.axdar.finstart.utilits.APP_ACTIVITY
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var appBarConfiguration: AppBarConfiguration
+    //private lateinit var appBarConfiguration: AppBarConfiguration
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -30,17 +31,17 @@ class MainActivity : AppCompatActivity() {
 
         APP_ACTIVITY = this
 
-        //setFragment(TradeQuotesFragment.newInstance())
-        //setListeners()
+        setFragment(TradeQuotesFragment.newInstance())
+        setListeners()
 
-        setSupportActionBar(toolbar)
-        initNav()
-        initDrawerToggle()
+        //setSupportActionBar(toolbar)
+        //initNav()
+        //initDrawerToggle()
     }
 
     private fun setFragment(fragment: Fragment) {
         supportFragmentManager.beginTransaction()
-            .replace(R.id.nav_host_fragment, fragment)
+            .replace(R.id.container_main, fragment)
             .commit()
     }
 
@@ -59,12 +60,16 @@ class MainActivity : AppCompatActivity() {
                     setFragment(TransactionsFragment())
                     true
                 }
+                R.id.menu_planner -> {
+                    setFragment(NavigationDrawerFragment())
+                    true
+                }
                 else -> false
             }
         }
     }
 
-    private fun initNav() {
+    /*private fun initNav() {
         val drawerLayout: DrawerLayout = findViewById(R.id.drawer_layout)
         val navView: NavigationView = findViewById(R.id.nav_view)
         val bottomNavView: BottomNavigationView = findViewById(R.id.bottom_nav_main)
@@ -76,11 +81,11 @@ class MainActivity : AppCompatActivity() {
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
-        bottomNavView.setupWithNavController(navController)
-        bottomNavView.setOnNavigationItemSelectedListener(onBottomNavigationItemSelectedListener)
-    }
+        //bottomNavView.setupWithNavController(navController)
+        //bottomNavView.setOnNavigationItemSelectedListener(onBottomNavigationItemSelectedListener)
+    }*/
 
-    override fun onSupportNavigateUp(): Boolean {
+    /*override fun onSupportNavigateUp(): Boolean {
         val navController = findNavController(R.id.nav_host_fragment)
         return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
     }
@@ -94,9 +99,9 @@ class MainActivity : AppCompatActivity() {
             }
             else -> super.onOptionsItemSelected(item)
         }
-    }
+    }*/
 
-    private fun initDrawerToggle() {
+    /*private fun initDrawerToggle() {
         //без этого вместо "гамбургера" будет стрелка
         val drawerToggle = ActionBarDrawerToggle(
             this, drawer_layout, R.string.drawer_open, R.string.drawer_close
@@ -104,17 +109,17 @@ class MainActivity : AppCompatActivity() {
         drawer_layout.addDrawerListener(drawerToggle)
         drawerToggle.syncState()
         //supportActionBar?.setDisplayHomeAsUpEnabled(true)
-    }
+    }*/
 
-    override fun onBackPressed() {
+    /*override fun onBackPressed() {
         if (drawer_layout.isDrawerOpen(GravityCompat.START)) {
             drawer_layout.closeDrawer(GravityCompat.START)
         } else {
             super.onBackPressed()
         }
-    }
+    }*/
 
-    private val onBottomNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
+    /*private val onBottomNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
         item.isChecked = true
         when (item.itemId) {
             R.id.menu_quotes -> {
@@ -128,5 +133,5 @@ class MainActivity : AppCompatActivity() {
             }
         }
         false
-    }
+    }*/
 }
